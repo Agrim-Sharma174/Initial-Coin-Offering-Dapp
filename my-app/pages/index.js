6d6d4414f5c8a3e0bde7880092a1d1a11fcbaca3
Agrim <agrimsharma1714@gmail.com>
2023-01-05 23:24:11 +0530

    Add connectWallet and button for it

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,11 +1,44 @@
 import Head from "next/head";
-import { useEffect, useRef } from "react";
-import { Web3Modal } from "web3modal";
+import { useEffect, useRef, useState } from "react";
+import styles from "../styles/Home.module.css";
+import  Web3Modal, { providers } from "web3modal";
+import { BigNumber, utils } from "ethers";
 
 export default function Home() {
 
+  const zero = BigNumber.from(0);
   const[walletConnected, setWalletConnected] = useState(false);
   const web3ModalRef = useRef();
+  const {tokensMinted, setTokensMinted} = useState(zero);
+  const {balanceOfASCryptoDevTokens, setBalanceOfASCryptoDevTokens} = useState(zero);
+
+  const getProviderOrSigner = async(needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.web3Provider(provider);
+
+    const {chainId} = await web3Provider.getNetwork();
+
+    if(chainId !== 5){
+      window.alert("Please connect to Goerli Testnet");
+      throw new Error("Please connect to Goerli Testnet");
+    }
+
+    if(needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+
+    return web3Provider;
+  };
+  
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true)
+    } catch(err){
+      console.error(err)
+    }
+  }
 
   useEffect(()=> {
     if(!walletConnected){
@@ -13,9 +46,10 @@ export default function Home() {
         network: "goerli",
         providerOptions: {},
         disableInjectedProvider: false,
-      })
+      });
+      connectWallet()
     }
-  },[])
+  },[]);
   return (
     <div>
       <Head>
@@ -29,6 +63,21 @@ export default function Home() {
         <div className="{styles.description">
           You can claim or mint Agrim Crypto Dev tokens here
         </div>
+        {
+          walletConnected ? (
+          <div>
+          <div className={styles.description}>
+            You have minted {utils.formatEther(balanceOfASCryptoDevTokens)} Agrim Crypto Dev tokens
+          </div>
+            <div className={styles.description}>
+              Overall {utils.formatEther(tokensMinted)}/10000 have been minted
+            </div>
+          </div>
+          ) : (
+          <button onClick={connectWallet} className ={styles.button}>
+            Connect your Wallet
+          </button>
+        )}
       </div>
     </div>
   );
