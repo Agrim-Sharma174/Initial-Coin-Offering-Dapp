2cc32c1f8e8593bf510c4273174571037c6ded3d
Agrim <agrimsharma1714@gmail.com>
2023-01-06 14:23:49 +0530

    Add constants folder and file

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,55 +1,87 @@
 import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
-import  Web3Modal, { providers } from "web3modal";
+import Web3Modal, { providers } from "web3modal";
 import { BigNumber, utils } from "ethers";
 
 export default function Home() {
-
   const zero = BigNumber.from(0);
-  const[walletConnected, setWalletConnected] = useState(false);
+  const [walletConnected, setWalletConnected] = useState(false);
   const web3ModalRef = useRef();
-  const {tokensMinted, setTokensMinted} = useState(zero);
-  const {balanceOfASCryptoDevTokens, setBalanceOfASCryptoDevTokens} = useState(zero);
+  const [tokensMinted, setTokensMinted] = useState(zero);
+  const [balanceOfASCryptoDevTokens, setBalanceOfASCryptoDevTokens] =
+    useState(zero);
+  const [tokenAmount, setTokenAmount] = useState(zero);
 
-  const getProviderOrSigner = async(needSigner = false) => {
+  const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.web3Provider(provider);
 
-    const {chainId} = await web3Provider.getNetwork();
+    const { chainId } = await web3Provider.getNetwork();
 
-    if(chainId !== 5){
+    if (chainId !== 5) {
       window.alert("Please connect to Goerli Testnet");
       throw new Error("Please connect to Goerli Testnet");
     }
 
-    if(needSigner) {
+    if (needSigner) {
       const signer = web3Provider.getSigner();
       return signer;
     }
 
     return web3Provider;
   };
-  
+
   const connectWallet = async () => {
     try {
       await getProviderOrSigner();
-      setWalletConnected(true)
-    } catch(err){
-      console.error(err)
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const mintCryptoDevToken = async(amount) => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const tokenContract = new Contract(
+        
+      )
+    } catch (error) {
+      console.error(err);
     }
   }
+  const renderButton = () => {
+    return (
+      <div style={{ display: "flex-col" }}>
+        <div>
+          <input
+            type="number"
+            placeholder="Amount of Tokens"
+            onChange={(e) => setTokenAmount(BigNumber.from(e.target.value))}
+          />
+          <button
+            className={styles.button}
+            disabled={!(tokenAmount > 0)}
+            onClick={() => mintCryptoDevToken(tokenAmount)}
+          >
+            Mint Token
+          </button>
+        </div>
+      </div>
+    );
+  };
 
-  useEffect(()=> {
-    if(!walletConnected){
+  useEffect(() => {
+    if (!walletConnected) {
       web3ModalRef.current = new Web3Modal({
         network: "goerli",
         providerOptions: {},
         disableInjectedProvider: false,
       });
-      connectWallet()
+      connectWallet();
     }
-  },[]);
+  }, []);
   return (
     <div>
       <Head>
@@ -63,18 +95,19 @@ export default function Home() {
         <div className="{styles.description">
           You can claim or mint Agrim Crypto Dev tokens here
         </div>
-        {
-          walletConnected ? (
+        {walletConnected ? (
           <div>
-          <div className={styles.description}>
-            You have minted {utils.formatEther(balanceOfASCryptoDevTokens)} Agrim Crypto Dev tokens
-          </div>
+            <div className={styles.description}>
+              You have minted {utils.formatEther(balanceOfASCryptoDevTokens)}{" "}
+              Agrim Crypto Dev tokens
+            </div>
             <div className={styles.description}>
               Overall {utils.formatEther(tokensMinted)}/10000 have been minted
             </div>
+            {renderButton}
           </div>
-          ) : (
-          <button onClick={connectWallet} className ={styles.button}>
+        ) : (
+          <button onClick={connectWallet} className={styles.button}>
             Connect your Wallet
           </button>
         )}
