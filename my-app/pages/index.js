ec4547740f9c6c9bbd6024827bd72ecb30d99a44
Agrim <agrimsharma1714@gmail.com>
2023-01-08 17:23:16 +0530

    add image and completed the project

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,8 +1,14 @@
+import { BigNumber, Contract, providers, utils } from "ethers";
 import Head from "next/head";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import {
+  NFT_CONTRACT_ABI,
+  NFT_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+  TOKEN_CONTRACT_ADDRESS,
+} from "../constants";
 import styles from "../styles/Home.module.css";
-import Web3Modal, { providers } from "web3modal";
-import { BigNumber, utils } from "ethers";
 
 export default function Home() {
   const zero = BigNumber.from(0);
@@ -15,10 +21,9 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [tokensToBeClaimed, settokensToBeClaimed] = useState(zero);
 
-
   const getProviderOrSigner = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.web3Provider(provider);
+    const web3Provider = new providers.Web3Provider(provider);
 
     const { chainId } = await web3Provider.getNetwork();
 
@@ -44,26 +49,43 @@ export default function Home() {
     }
   };
 
-  const getTokensToBeClaimed = async() => {
+  const getTokensToBeClaimed = async () => {
     try {
       const provider = await getProviderOrSigner();
       const nftContract = new Contract(
         NFT_CONTRACT_ADDRESS,
         NFT_CONTRACT_ABI,
         provider
+      );
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        provider
       )
       const signer = await getProviderOrSigner(true);
       const address = await signer.getAddress();
       const balance = await nftContract.balanceOf(address);
 
-      
-      
-    } catch (error) {
-      console.error(err);
-    }
-  }
+      if (balance === zero) {
+        settokensToBeClaimed(zero);
+      } else {
+        var amount = 0;
 
-  const getBalanceOfASCryptoDevTokens = async() => {
+        for (var i = 0; i < balance; i++) {
+          const tokenId = await nftContract.tokenOfOwnerByIndex(address, i);
+          const claimed = await tokenContract.tokenIdsClaimed(tokenId);
+          if (!claimed) {
+            amount++;
+          }
+        }
+        settokensToBeClaimed(BigNumber.from(amount));
+      }
+    } catch (error) {
+      settokensToBeClaimed(zero);
+    }
+  };
+
+  const getBalanceOfASCryptoDevTokens = async () => {
     try {
       const provider = await getProviderOrSigner();
       const tokenContract = new Contract(
@@ -71,34 +93,32 @@ export default function Home() {
         TOKEN_CONTRACT_ABI,
         provider
       );
-      
-      const signer = getProviderOrSigner(true);
+
+      const signer = await getProviderOrSigner(true);
       const address = signer.getAddress();
       const balance = await tokenContract.balanceOf(address);
       setBalanceOfASCryptoDevTokens(balance);
-
-    } catch (error) {
+    } catch (err) {
       console.error(err);
     }
   };
 
-  const getTotalTokensMinted = async() => {
+  const getTotalTokensMinted = async () => {
     try {
       const provider = await getProviderOrSigner();
       const tokenContract = new Contract(
         TOKEN_CONTRACT_ADDRESS,
         TOKEN_CONTRACT_ABI,
         provider
-      )
+      );
       const _tokensMinted = await tokenContract.totalSupply();
       setTokensMinted(_tokensMinted);
-    } catch (error) {
+    } catch (err) {
       console.error(err);
     }
-  }
+  };
 
-
-  const mintCryptoDevToken = async(amount) => {
+  const mintCryptoDevToken = async (amount) => {
     try {
       const signer = await getProviderOrSigner(true);
       const tokenContract = new Contract(
@@ -107,35 +127,66 @@ export default function Home() {
         signer
       );
 
-      const value = 0.001*amount;
+      const value = 0.001 * amount;
 
-      const tx = await tokenContract.mint(amount,{
-        value: utils.parseEther(value.toString())
-      })
+      const tx = await tokenContract.mint(amount, {
+        value: utils.parseEther(value.toString()),
+      });
       setLoading(true);
       await tx.wait();
       setLoading(false);
-      window.alert("Successfully minted Agrim Crypto Dev Tokens")
+      window.alert("Successfully minted Agrim Crypto Dev Tokens");
 
       await getBalanceOfASCryptoDevTokens();
       await getTotalTokensMinted();
+      await getTokensToBeClaimed();
     } catch (error) {
       console.error(err);
     }
   };
+
+  const claimASCryptoDevTokens = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const tokenContract = new Contract(
+        TOKEN_CONTRACT_ADDRESS,
+        TOKEN_CONTRACT_ABI,
+        signer
+      );
+      const tx = await tokenContract.claim();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      window.alert("Successfully claimed Agrim Crypto Dev Tokens");
+      await getBalanceOfASCryptoDevTokens();
+      await getTotalTokensMinted();
+      await getTokensToBeClaimed();
+    } catch (error) {
+      console.error(err);
+    }
+  };
+
   const renderButton = () => {
-    if(loading) {
+    if (loading) {
       return (
         <div>
-          <button className={styles.button}>
-            Loading...
-          </button>
+          <button className={styles.button}>Loading...</button>
         </div>
-      )
+      );
     }
 
-    if(tokensToBeClaimed){
+    if (tokensToBeClaimed > 0) {
+      return (
+        <div>
+          <div className="{styles.description">
+            {tokensToBeClaimed * 10} Tokens can be Claimed!
+          </div>
 
+          <button className={styles.button} onClick={claimASCryptoDevTokens}>
+            Claim Tokens
+          </button>
+        </div>
+      );
     }
 
     return (
@@ -166,38 +217,50 @@ export default function Home() {
         disableInjectedProvider: false,
       });
       connectWallet();
+      getBalanceOfASCryptoDevTokens();
+      getTotalTokensMinted();
+      getTokensToBeClaimed();
     }
-  }, []);
+  }, [walletConnected]);
   return (
     <div>
       <Head>
-        <title>Agrim Crypto Devs ICO</title>
-        <meta name="description" content="ICO-dApp" />
-        <link rel="icon" href="./favicon.ico" />
+        <title>Agrim Crypto Devs</title>
+        <meta name="description" content="ICO-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="{styles.main}">
-        <h1 className="{styles.title}"> Welcome to Agrim Crypto Devs ICO </h1>
-        <div className="{styles.description">
-          You can claim or mint Agrim Crypto Dev tokens here
-        </div>
-        {walletConnected ? (
-          <div>
-            <div className={styles.description}>
-              You have minted {utils.formatEther(balanceOfASCryptoDevTokens)}{" "}
-              Agrim Crypto Dev tokens
-            </div>
-            <div className={styles.description}>
-              Overall {utils.formatEther(tokensMinted)}/10000 have been minted
-            </div>
-            {renderButton}
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Agrim Crypto Devs ICO!</h1>
+          <div className={styles.description}>
+            You can claim or mint Agrim Crypto Dev tokens here
           </div>
-        ) : (
-          <button onClick={connectWallet} className={styles.button}>
-            Connect your Wallet
-          </button>
-        )}
+          {walletConnected ? (
+            <div>
+              <div className={styles.description}>
+                {/* Format Ether helps us in converting a BigNumber to string */}
+                You have minted {utils.formatEther(balanceOfASCryptoDevTokens)}{" "}
+                Agrim Crypto Dev tokens
+              </div>
+              <div className={styles.description}>
+                {/* Format Ether helps us in converting a BigNumber to string */}
+                Overall {utils.formatEther(tokensMinted)}/10000 have been
+                minted!!!
+              </div>
+              {renderButton()}
+            </div>
+          ) : (
+            <button onClick={connectWallet} className={styles.button}>
+              Connect your Wallet
+            </button>
+          )}
+        </div>
+        <div>
+          <img className={styles.image} src="./0.svg" />
+        </div>
       </div>
+      <footer className={styles.footer}>Made with &#10084; by Agrim</footer>
     </div>
   );
 }
---
+++ my-app/pages/test.js
@@ -0,0 +1,50 @@
+<div>
+      <Head>
+        <title>Agrim Crypto Devs</title>
+        <meta name="description" content="ICO-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Agrim Crypto Devs ICO!</h1>
+          <div className={styles.description}>
+            You can claim or mint Agrim Crypto Dev tokens here
+          </div>
+          {walletConnected ? (
+            <div>
+              <div className={styles.description}>
+                {/* Format Ether helps us in converting a BigNumber to string */}
+                You have minted {utils.formatEther(balanceOfASCryptoDevTokens)} Agrim Crypto Dev tokens
+              </div>
+              <div className={styles.description}>
+                {/* Format Ether helps us in converting a BigNumber to string */}
+                Overall {utils.formatEther(tokensMinted)}/10000 have been minted!!!
+              </div>
+              {renderButton()}
+              {/* Display additional withdraw button if connected wallet is owner */}
+                {isOwner ? (
+                  <div>
+                  {loading ? <button className={styles.button}>Loading...</button>
+                           : <button className={styles.button} onClick={withdrawCoins}>
+                               Withdraw Coins
+                             </button>
+                  }
+                  </div>
+                  ) : ("")
+                }
+            </div>
+          ) : (
+            <button onClick={connectWallet} className={styles.button}>
+              Connect your wallet
+            </button>
+          )}
+        </div>
+        <div>
+          <img className={styles.image} src="./0.svg" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#10084; by Crypto Devs
+      </footer>
+    </div>
